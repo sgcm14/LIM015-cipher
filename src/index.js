b94fb5a0790dc419a9f8eb17fae5045f2a40b988
sgcm14 <sgcm14@gmail.com>
2021-06-03 12:38:27 -0500

    cambios en html, js y css

--- src/index.js
+++ src/index.js
@@ -4,22 +4,36 @@ import cipher from './cipher.js';
 
 console.log(cipher.decode("hola mundo"));
 
-console.log("CIFRANDO");
+console.log("probando.........");
 let frase = 'ABCDef G';
 let offset = 7;
-
 console.log("Texto:");
 console.log(frase);
+
+console.log("");
+console.log("CIFRANDO:");
+console.log("--------------------");
 let array="";
 let nuevaFrase="";
-console.log("Nuevo Texto:");
 for(let i=0;i<frase.length;i++){
     //obtiene el caracter en unicode y le suma el offset
     array=(frase.charCodeAt(i)+offset);
     //recibe el nuevo unicode y lo tansforma a letra
     nuevaFrase+=String.fromCharCode(array);
-    console.log(nuevaFrase);
 }
+console.log(nuevaFrase);
 
+console.log("");
+console.log("DESCIFRANDO:");
+console.log("--------------------");
+let array2="";
+let nuevaFrase2="";
+for(let i=0;i<nuevaFrase.length;i++){
+    //obtiene el caracter en unicode y le suma el offset
+    array2=(nuevaFrase.charCodeAt(i)-offset);
+    //recibe el nuevo unicode y lo tansforma a letra
+    nuevaFrase2+=String.fromCharCode(array2);
+}
+console.log(nuevaFrase2);
 
 
