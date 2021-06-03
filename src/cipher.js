05cadfcc20a8ae9b989a3a5a1935a73167f4858e
sgcm14 <sgcm14@gmail.com>
2021-06-03 09:04:29 -0500

    probando js

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,10 @@
 const cipher = {
   // ...
+  encode:() =>{},
+  decode:(mensaje) => {return mensaje}
+
 };
 
+
+//transformar a mensaje cifrado
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import cipher from './cipher.js';
+//elegir los elementos que usare del html
+//console.log(cipher);
 
-console.log(cipher);
+console.log(cipher.decode("hola mundo"));
+
+console.log("CIFRANDO");
+let frase = 'ABCDef G';
+let offset = 7;
+
+console.log("Texto:");
+console.log(frase);
+let array="";
+let nuevaFrase="";
+console.log("Nuevo Texto:");
+for(let i=0;i<frase.length;i++){
+    //obtiene el caracter en unicode y le suma el offset
+    array=(frase.charCodeAt(i)+offset);
+    //recibe el nuevo unicode y lo tansforma a letra
+    nuevaFrase+=String.fromCharCode(array);
+    console.log(nuevaFrase);
+}
+
+
+
