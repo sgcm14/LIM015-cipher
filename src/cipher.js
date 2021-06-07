66e209cf83b38339631d05978d2e2665566e5d8b
sgcm14 <sgcm14@gmail.com>
2021-06-07 18:31:41 -0500

    código ordenado y correcto

--- src/cipher.js
+++ src/cipher.js
@@ -1,50 +1,36 @@
 const cipher = {
-  // ...
+  
   encode:(offset,frase) =>{
-    console.log(frase);
-    console.log(offset);
-    console.log("");
-    console.log("CIFRANDO:");
-    console.log("--------------------");
     let indiceLetra="";
     let nuevaFrase="";
     for(let i=0;i<frase.length;i++){
-        //obtiene el caracter en unicode y le suma el offset
-        indiceLetra=(frase.charCodeAt(i)+parseInt(offset));
+      //obtiene el caracter en unicode y le suma el offset
+      indiceLetra=(frase.charCodeAt(i)+parseInt(offset));
         if(indiceLetra>90){
             indiceLetra=(indiceLetra-90)+64;
         }
-        //recibe el nuevo unicode y lo tansforma a letra
-        nuevaFrase+=String.fromCharCode(indiceLetra);
+      //recibe el nuevo unicode y lo tansforma a letra
+      nuevaFrase+=String.fromCharCode(indiceLetra);
     }
-        //console.log(nuevaFrase);
-        return nuevaFrase;
+    return nuevaFrase;
   },
-
 
   decode:(offset,frase) => {
-    console.log(frase);
-    console.log(offset);
-    console.log("DESCIFRANDO:");
-    console.log("--------------------");
-    let indiceLetra2="";
-    let nuevaFrase2="";
+    let indiceLetra="";
+    let nuevaFrase="";
     for(let i=0;i<frase.length;i++){
-        //obtiene el caracter en unicode y le suma el offset
-        indiceLetra2=(frase.charCodeAt(i)-parseInt(offset));
-        if(indiceLetra2<65){
-            indiceLetra2=(91-(65-indiceLetra2))
+      //obtiene el caracter en unicode y le suma el offset
+      indiceLetra=(frase.charCodeAt(i)-parseInt(offset));
+        if(indiceLetra<65){
+            indiceLetra=(91-(65-indiceLetra))
         }
-        //recibe el nuevo unicode y lo tansforma a letra
-        nuevaFrase2+=String.fromCharCode(indiceLetra2);
+      //recibe el nuevo unicode y lo tansforma a letra
+      nuevaFrase+=String.fromCharCode(indiceLetra);
     }
-    //console.log(nuevaFrase2);
-    return nuevaFrase2;
+    return nuevaFrase;
   },
-
 
 };
 
-
-//transformar a mensaje cifrado
+//envía todo lo que esta dentro de cipher 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import cipher from './cipher.js';
 //elegir los elementos que usare del html
 
-
+document.getElementById('offset').focus();
 
 const btnLimpiar = document.getElementById('btnLimpiar');
 btnLimpiar.addEventListener('click', () => {
     document.getElementById('offset').value ="";
     document.getElementById('mensaje').value ="";
     document.getElementById('mensajeResultado').value ="";
+    document.getElementById('offset').focus();
 });
 
 const btnCifrar = document.getElementById('btnCifrar');
@@ -16,12 +17,10 @@ btnCifrar.addEventListener('click', () => {
     let offset =document.getElementById('offset').value;    
     let frase=document.getElementById('mensaje').value;
     
-    let mensaje = (cipher.encode(offset,frase));
+    let resultado = (cipher.encode(offset,frase));
 
-    document.getElementById('mensajeResultado').value = mensaje;
-    
+    document.getElementById('mensajeResultado').value = resultado;
 });
-
 
 const btnDescifrar = document.getElementById('btnDescifrar');
 btnDescifrar.addEventListener('click', () => {
@@ -29,8 +28,7 @@ btnDescifrar.addEventListener('click', () => {
     let offset =document.getElementById('offset').value;    
     let frase=document.getElementById('mensaje').value;
     
-    let mensaje = (cipher.decode(offset,frase));
+    let resultado = (cipher.decode(offset,frase));
 
-    document.getElementById('mensajeResultado').value = mensaje;
-    
+    document.getElementById('mensajeResultado').value = resultado;
 });
