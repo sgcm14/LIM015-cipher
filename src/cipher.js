d3cdc4b5c19df8d84d15790baab7165baba4d012
sgcm14 <sgcm14@gmail.com>
2021-06-06 23:31:07 -0500

    funcion encode

--- src/cipher.js
+++ src/cipher.js
@@ -1,7 +1,29 @@
 const cipher = {
   // ...
-  encode:() =>{},
-  decode:(mensaje) => {return mensaje}
+  encode:(offset,frase) =>{
+    //console.log(frase);
+    //console.log(offset);
+    console.log("");
+    console.log("CIFRANDO:");
+    console.log("--------------------");
+    let indiceLetra="";
+    let nuevaFrase="";
+    for(let i=0;i<frase.length;i++){
+        //obtiene el caracter en unicode y le suma el offset
+        indiceLetra=(frase.charCodeAt(i)+parseInt(offset));
+        if(indiceLetra>90){
+            indiceLetra=(indiceLetra-90)+64;
+        }
+        //recibe el nuevo unicode y lo tansforma a letra
+        nuevaFrase+=String.fromCharCode(indiceLetra);
+    }
+        //console.log(nuevaFrase);
+        return nuevaFrase;
+  },
+
+
+  decode:(mensaje) => {return mensaje;},
+
 
 };
 
--- src/index.js
+++ src/index.js
@@ -2,38 +2,43 @@ import cipher from './cipher.js';
 //elegir los elementos que usare del html
 //console.log(cipher);
 
-console.log(cipher.decode("hola mundo"));
+//console.log(cipher.decode("hola mundo"));
 
+
+
+
+
+const btn = document.getElementById('btnCifrar');
+btn.addEventListener('click', () => {
+    let frase= "ABC";
+    let offset =document.getElementById('offset').value;
+    //console.log(frase);
+    console.log(cipher.encode(offset,frase));
+});
+
+
+//de aqui a abajo es prueba
+/*console.log("--------------------");
 console.log("probando.........");
-let frase = 'ABCDef G';
-let offset = 7;
+let frase = 'HOLAMUNDO';
+let offsetPrueba = 5;
 console.log("Texto:");
-console.log(frase);
+console.log(frase);*/
 
-console.log("");
-console.log("CIFRANDO:");
-console.log("--------------------");
-let array="";
-let nuevaFrase="";
-for(let i=0;i<frase.length;i++){
-    //obtiene el caracter en unicode y le suma el offset
-    array=(frase.charCodeAt(i)+offset);
-    //recibe el nuevo unicode y lo tansforma a letra
-    nuevaFrase+=String.fromCharCode(array);
-}
-console.log(nuevaFrase);
 
-console.log("");
+
+/*console.log("");
 console.log("DESCIFRANDO:");
 console.log("--------------------");
-let array2="";
+let indiceLetra2="";
 let nuevaFrase2="";
 for(let i=0;i<nuevaFrase.length;i++){
     //obtiene el caracter en unicode y le suma el offset
-    array2=(nuevaFrase.charCodeAt(i)-offset);
+    indiceLetra2=(nuevaFrase.charCodeAt(i)-offsetPrueba);
+    if(indiceLetra2<65){
+        indiceLetra2=(91-(65-indiceLetra2))
+    }
     //recibe el nuevo unicode y lo tansforma a letra
-    nuevaFrase2+=String.fromCharCode(array2);
+    nuevaFrase2+=String.fromCharCode(indiceLetra2);
 }
-console.log(nuevaFrase2);
-
-
+console.log(nuevaFrase2);*/
